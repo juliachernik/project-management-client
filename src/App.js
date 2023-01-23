3e6deb1f1512a471af5cdaab0e6f670b3b23a857
juliachernik <116951695+juliachernik@users.noreply.github.com>
2023-01-23 20:18:21 +0100

    Implement CRUD

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom"; // <== IMPORT
+ 
+import Navbar from "./components/Navbar";     // <== IMPORT
+import HomePage from "./pages/HomePage";     // <== IMPORT
+import ProjectListPage from "./pages/ProjectListPage"; 
+import ProjectDetailsPage from "./pages/ProjectDetailsPage"; 
+import EditProjectPage from "./pages/EditProjectPage"; 
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     {/* Below: ADD <Navbar>, <Routes> & <Route> */}
+      <Navbar />
+ 
+      <Routes>      
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="/projects" element={<ProjectListPage />} />
+        <Route path="/projects/:projectId" element={<ProjectDetailsPage />} />   
+        <Route path="/projects/edit/:projectId" element={ <EditProjectPage /> } />    
+      </Routes>
+      
     </div>
   );
 }
-
+ 
 export default App;
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
